525025a32e5ab4e88f08359328f8edfe2a8b8a04
Patrick Camacho <patrick@crashlytics.com>
2013-04-02 13:31:19 -0400

    added base styling

--- versus-web/app.js
+++ versus-web/app.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server);
+var path = require('path');
 
 server.listen(3000);
 
 app.use(express.bodyParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
